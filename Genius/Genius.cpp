1336e3738a0dd70e39b8cab124b7a64d844e946b
Dylan Gentile <dylangentile@ymail.com>
2018-11-14 16:50:48 -0800

    Generic Message; git push jfksfjks

---
+++ Genius/Genius.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define BUFSIZE 1000
+
+int main(int argc, char const *argv[])
+{
+	
+
+    FILE *fp = fopen(argv[1], "r"); 
+
+    char buff[BUFSIZE]; 
+    while(fgets(buff, BUFSIZE - 1, fp) != NULL) 
+    {
+       
+
+        printf ("%s\n", buff); 
+    }
+    fclose(fp);  
+
+	return 0;
+}
